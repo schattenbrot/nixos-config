7251a3c3d5bcbbff97b4b38945c6908b41b5da14
Schattenbrot <aaron.machill@gmail.com>
2025-12-19 21:22:39 +0100

    fix fish-tide command coloring

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -25,6 +25,9 @@
       set fish_greeting
       setenv EDITOR vim
       setenv VISUAL vim
+      set -U fish_color_command blue
+      set -U fish_color_builtin blue
+      set -U fish_color_function blue
       eval (ssh-agent -c 2>/dev/null | grep -v '^echo')
       ssh-add ~/.ssh/github_ed25519 > /dev/null 2>&1
     '';
